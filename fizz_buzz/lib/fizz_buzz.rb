a07fa6c73cd7b0dfe43f58d1235596d33bff2444
danmalmx <danmalmx@gmail.com>
2018-08-15 12:08:57 +0200

    fizz_buzz, incomplete,  upload

---
+++ fizz_buzz/lib/fizz_buzz.rb
@@ -0,0 +1,28 @@
+def fizz_buzz (num)
+    if num % 3 == 0 
+        prints "fizz"
+    elsif num % 5 == 0
+        prints "buzz"
+    elsif num 15 == 0   
+        prints "fizz buzz"
+    else
+        num
+    end
+end
+
+=begin
+def fizz_buzz(num)
+    while num <= 100
+    if num % 3 == 0
+    puts "fizz" 
+elsif num % 5 == 0
+    puts "buzz" 
+elsif num % 3 && num % 5 == 0
+    puts "fizzbuzz"
+else
+    puts (num)
+end
+num += 1
+end
+end
+=end
