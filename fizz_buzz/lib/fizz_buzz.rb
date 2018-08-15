a5f6757cdfd86b3ae5868f721a35c7670d46d5fe
danmalmx <danmalmx@gmail.com>
2018-08-15 15:52:22 +0200

    fuzz_buzz completed

--- fizz_buzz/lib/fizz_buzz.rb
+++ fizz_buzz/lib/fizz_buzz.rb
@@ -1,28 +1,27 @@
 def fizz_buzz (num)
-    if num % 3 == 0 
-        prints "fizz"
-    elsif num % 5 == 0
-        prints "buzz"
-    elsif num 15 == 0   
-        prints "fizz buzz"
+    if 
+        num.class == String
+        "text not allowed"
+
+    elsif
+        num <= 0
+        "below 0"
+    
+    elsif
+        num % 15 == 0 
+        "fizz buzz"
+
+    elsif 
+        num % 5 == 0
+        "buzz"
+    
+    elsif 
+        num % 3 == 0   
+        "fizz"
+
+        
     else
         num
+    
     end
 end
-
-=begin
-def fizz_buzz(num)
-    while num <= 100
-    if num % 3 == 0
-    puts "fizz" 
-elsif num % 5 == 0
-    puts "buzz" 
-elsif num % 3 && num % 5 == 0
-    puts "fizzbuzz"
-else
-    puts (num)
-end
-num += 1
-end
-end
-=end
--- fizz_buzz/spec/fizz_buzz_spec.rb
+++ fizz_buzz/spec/fizz_buzz_spec.rb
@@ -1,19 +1,29 @@
 require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
+    
+    it "returns 'text not allowed' if number below or equal to num.instance_of? String" do
+        expect(fizz_buzz("Daniel")).to eq "text not allowed"
+    end  
+
+    it "returns 'fizz buzz' if number is divisible by 15" do
+        expect(fizz_buzz(15)).to eq "fizz buzz"
+    end   
+
+    it "returns 'fizz' if number is divisible by 5" do
+        expect(fizz_buzz(5)).to eq "buzz"
+    end   
+
+    it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(3)).to eq "fizz"
+    end
+
     it 'returns 1 if number is 1' do
         expect(fizz_buzz(1)).to eq 1 
     end
 
-    it "returns 'fizz' if number is divisible by 3" do
-        expect(fizz_buzz(3)).to eq "fizz"
-      end
-      
-      it "returns 'fizz' if number is divisible by 3" do
-        expect(fizz_buzz(3)).to eq "fizz"
-      end   
-      
-      it "returns 'fizz buzz' if number is divisible by 15" do
-        expect(fizz_buzz(15)).to eq "fizz buzz"
-      end   
+    it "returns 'below 0' if number below or equal to 0" do
+        expect(fizz_buzz(0)).to eq "below 0"
+    end  
+
 end
